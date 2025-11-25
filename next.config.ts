ccef441cb43f9f429af52bf51b712ce7f1235ae9
jvieir96 <jvieir96@mail.com>
2025-11-25 20:29:17 -0300

    revert next config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/jvieir96/biankiislic",
-  assetPrefix: "/jvieir96/biankiislic/",
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
